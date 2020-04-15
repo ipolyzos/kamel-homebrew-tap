76ff3acdd142254ff73fc8b37e4574d14309b24d
Ioannis Polyzos <i.polyzos@gmail.com>
2020-04-15 18:25:41 +0100

    initial commit

---
+++ kamel.rb
@@ -0,0 +1,38 @@
+class Kamel < Formula
+  desc "Apache Camel K is a lightweight integration platform, born on Kubernetes, with serverless superpowers."
+  homepage "https://camel.apache.org/"
+  
+  url "https://github.com/apache/camel-k.git",
+    :tag      => "1.0.0-RC2",
+    :revision => "24ddce5afb41c70a0bf30e06f74c6f380f5fd851"
+  head "https://github.com/apache/camel-k.git"
+  
+  bottle :unneeded
+
+  depends_on "java" 
+  depends_on "go"
+
+  def install
+      # Build from source & Install
+      system "make"
+      bin.install "kamel"
+
+      # Install bash completion
+      output = Utils.popen_read("#{bin}/kamel completion bash")
+      (bash_completion/"kamel").write output
+
+      prefix.install_metafiles
+
+      # Install zsh completion
+      output = Utils.popen_read("#{bin}/kamel completion zsh")
+      (zsh_completion/"_kamel").write output
+  end
+
+  test do
+    run_output = shell_output("#{bin}/kamel 2>&1")
+    assert_match "Apache Camel K is a lightweight integration platform, born on Kubernetes, with serverless superpowers."
+
+  version_output = shell_output("#{bin}/kamel version 2>&1")
+      assert_match "Camel K Client 1.0.0-RC2"
+  end
+end
